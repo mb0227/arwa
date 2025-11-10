834e28c4432a698c1d597b4a7e6840b33df7dbc8
mb0227 <bilalyaseen2k4@gmail.com>
2025-11-10 19:10:09 +0500

    try v4

--- server/server.js
+++ server/server.js
@@ -116,7 +116,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get('(.*)', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
